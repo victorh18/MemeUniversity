680e458f1ee847b5d8014763055b90513b85b12b
Victor de la Rosa <v.r.delarosa@outlook.com>
2017-08-03 17:05:36 -0400

    More shit

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+//DAL, or Data Access Layer
+namespace ContosoUniversity.DAL
+{  
+    //A DbContext class manages the functionality of a model
+    public class SchoolContext : DbContext
+    {
+        //This specifies the name of the connection string 
+        public SchoolContext() : base("SchoolContext")
+        {
+
+        }
+        //DbSets represent the tables in the database, and an entity represents a row
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Keeps the database from creating the tables with plural forms of the models
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    //Declaration of the model
+    public class Student
+    {
+        //Property of the models
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
